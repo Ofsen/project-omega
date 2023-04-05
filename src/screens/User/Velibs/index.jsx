5afe587929710c3d7c61cf43c6fab9ee1a16acb8
Ofsen <ofsensmailbox@protonmail.com>
2023-04-06 00:20:20 +0200

    Filters (#4)
    * add search by communes
    
    * add filter by renting and returning
    
    * searchbar not complete
    
    filters
    
    ---------
    
    Co-authored-by: Fairouz BEN FRAJ <fairouz.benfraj@ynov.com>

--- src/screens/User/Velibs/index.jsx
+++ src/screens/User/Velibs/index.jsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState, useCallback} from 'react';
 import styled, {useTheme} from 'styled-components';
-import {ActivityIndicator, FlatList, Text} from 'react-native';
+import {ActivityIndicator, FlatList, Text, TextInput} from 'react-native';
 import {UserLayout} from '../../../components/layout/UserLayout';
-import {callApi} from '../../../services/events';
+import {callApi, getCommunes} from '../../../services/events';
 import {useFocusEffect} from '@react-navigation/native';
 import {error} from '../../../utils/notifications';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {Button} from '../../../components/Button';
+import {Checkbox, Menu, Provider, Searchbar} from 'react-native-paper';
 import {Share} from 'react-native';
 import {useTranslation} from 'react-i18next';
 
@@ -16,6 +18,13 @@ const Velibs = () => {
   const [page, setPage] = useState(0);
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+
+  const [communes, setCommunes] = useState([]);
+  const [selectedCommune, setSelectedCommune] = useState(null);
+  const [rentingFilter, setRentingFilter] = useState(false);
+  const [returningFilter, setReturningFilter] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [menuVisible, setMenuVisible] = useState(false);
   const [available, setAvailable] = React.useState(false);
   const [location, setLocation] = useState(null);
 
@@ -23,8 +32,9 @@ const Velibs = () => {
 
   const fetchData = async () => {
     try {
-      const res = await callApi(size, page);
+      const res = await callApi(size, page, selectedCommune, rentingFilter, returningFilter, searchQuery);
       if (res.status === 200) {
+        console.log("logs", res.data.records);
         setData(prev => [...prev, ...res.data.records]);
       }
     } catch (err) {
@@ -40,17 +50,57 @@ const Velibs = () => {
     setPage(0);
   };
 
-  useEffect(() => {
-    fetchData();
-  }, [page]);
-
   useFocusEffect(
     useCallback(() => {
       setData([]);
       setLoading(true);
       setPage(0);
+      if (page === 0) fetchData();
     }, []),
   );
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  const fetchCommunes = async () => {
+    try {
+      const response = await getCommunes();
+      if (response) {
+        setCommunes(response.facets[0].facets);
+      }
+    } catch (err) {
+      error(err.message);
+    }
+  };
+  
+  useEffect(() => {
+    fetchCommunes();
+  }, []);
+
+  const updateFilters = (renting, returning) => {
+    setRentingFilter(renting);
+    setReturningFilter(returning);
+    refreshData();
+  };
+
+  const onChangeSearch = () => {
+    setData([]);
+    setPage(0);
+  };
+
+  const searchData = async () => {
+    try {
+      setData([]);
+      const res = await callApi(size, 0, selectedCommune, rentingFilter, returningFilter, searchQuery);
+      if (res.status === 200) {
+        console.log("logs", res.data.records);
+        setData(prev => [...prev, ...res.data.records]);
+      }
+    } catch (err) {
+      error(err.message);
+    }
+    if (page === 0) setLoading(false);
 
   useEffect(() => {
     const fields = {};
@@ -114,6 +164,63 @@ const Velibs = () => {
 
   return (
     <UserLayout title="Velib">
+      <SearchContainer>
+        <Searchbar
+          placeholder="Rechercher"
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+          onClearIconPress={() => {setData([]), setPage(0)}}
+        />
+        
+      </SearchContainer>
+      <Button
+          label="OK"
+          pressHandler={() => searchData()}
+        />
+      <SearchContainer>
+        <Menu
+          visible={menuVisible}
+          onDismiss={() => setMenuVisible(false)}
+          anchor={
+            <Button
+            label="Toutes les communes"
+            variant="blue"
+            pressHandler={() => setMenuVisible(true)}
+          />
+          }
+        >
+          <Menu.Item onPress={() => {
+            setSelectedCommune(null);
+            setPage(0);
+            setData([]);
+            setMenuVisible(false);
+          }} title="Toutes les communes" />
+          {communes.map((commune, index) => (
+            <Menu.Item
+              key={index}
+              onPress={() => {
+                setSelectedCommune(commune.name);
+                setPage(0);
+                setData([]);
+                setMenuVisible(false);
+              }}
+              title={commune.name}
+            />
+          ))}
+        </Menu>
+      </SearchContainer>
+      <FilterContainer>
+      <Checkbox.Item
+        label="Louer"
+        status={rentingFilter ? 'checked' : 'unchecked'}
+        onPress={() => updateFilters(!rentingFilter, returningFilter)}
+      />
+      <Checkbox.Item
+        label="Retourner"
+        status={returningFilter ? 'checked' : 'unchecked'}
+        onPress={() => updateFilters(rentingFilter, !returningFilter)}
+      />
+      </FilterContainer>
       {loading ? (
         <Centered>
           <ActivityIndicator size="large" color={theme.red} />
@@ -190,6 +297,17 @@ const Centered = styled.View`
   justify-content: center;
   align-items: center;
 `;
+const SearchContainer = styled.View`
+  padding: 8px 16px;
+  flex-direction: row;
+  align-items: center;
+`;
+const FilterContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 16px;
+`;
 
 const Button = styled.TouchableOpacity`
   background-color: ${({theme}) => theme.primary};
@@ -198,4 +316,8 @@ const Button = styled.TouchableOpacity`
   margin-top: 70px;
 `;
 
-export default Velibs;
+export default () => (
+  <Provider>
+    <Velibs />
+  </Provider>
+);
--- src/services/events.js
+++ src/services/events.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import {DATA_API, API_URL} from '@env';
+import {DATA_API, API_URL, ARRONDISSEMENT_API} from '@env';
 import {getLang} from '../utils/lang/languageDetector';
+
 
 export const getPaginatedEvents = async (limit = 20, offset) => {
   return axios.get(`${DATA_API}`, {
@@ -22,13 +23,47 @@ export const getSingleEvent = async eventId => {
   });
 };
 
-export const callApi = async (row = 20, start) => {
-  return await axios.get(`${API_URL}`, {
-    params: {
-      row: row,
-      start: start * row,
-      timezone: 'Europe/Paris',
-      lang: await getLang(),
+export const callApi = async (
+  row = 20,
+  start,
+  commune,
+  rentingFilter,
+  returningFilter,
+  searchQuery,
+) => {
+  const params = {
+    row: row,
+    start: start * row,
+    timezone: 'Europe/Paris',
+    lang: await getLang(),
+    q: '',
+    refine: {
+      nom_arrondissement_communes: '',
+      is_renting: '',
+      is_returning: '',
     },
-  });
+  };
+
+  if (commune) {
+    params['refine.nom_arrondissement_communes'] = commune;
+  }
+  if (rentingFilter) {
+    params['refine.is_renting'] = 'OUI';
+  }
+  if (returningFilter) {
+    params['refine.is_returning'] = 'OUI';
+  }
+  if (searchQuery) {
+    params.q = searchQuery;
+  }
+
+  console.log(start, params.start);
+  const a = await axios.get(`${API_URL}`, {params});
+  console.log('pipipoupou', a);
+  return a;
+};
+
+export const getCommunes = async () => {
+  const response = await axios.get(`${ARRONDISSEMENT_API}`);
+  return response.data;
 };
